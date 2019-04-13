9f5e3476fff2218ee04f7e7f4060b0a3c12ee63d
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-13 14:09:12 -0600

    adds topic and followed_topic, updates favorited_articles table to reference topic

--- db/migrate/20190413192850_create_favorited_articles.rb
+++ db/migrate/20190413192850_create_favorited_articles.rb
@@ -3,6 +3,7 @@ class CreateFavoritedArticles < ActiveRecord::Migration[5.2]
     create_table :favorited_articles do |t|
       t.references :article, foreign_key: true
       t.references :user, foreign_key: true
+      t.references :topic, foreign_key: true
     end
   end
 end
