d0081da1b388c7c46f614a5965b334ee9cceec24
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-13 13:35:16 -0600

    adds initial User model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates_presence_of :name, :username
+  validates :email, presence: true, uniqueness: true
+
+  has_many :favorited_articles
+  has_many :articles, through: :favorited_articles
+  has_many :followed_topics
+  has_many :topics, through: :followed_topics
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,13 @@ describe User, type: :model do
     it {should have_secure_password}
     it {should validate_presence_of(:username)}
     it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+    it {should validate_presence_of(:password)}
   end
 
   describe 'relationships' do
-    it {should have_many(:articles).through(:user_articles)}
+    it {should have_many(:articles).through(:favorited_articles)}
     it {should have_many(:topics).through(:followed_topics)}
   end
 end
