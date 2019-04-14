e75250ca1b50313391df232962fc9e68bb40a2ac
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-14 11:35:31 -0600

    adds spec for instantiating an Article with JSON, adds helper method in AppHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def self.single_article
+    {
+      :source=>{:id=>"engadget", :name=>"Engadget"},
+      :author=>"Richard Lawler",
+      :title=>"Watch SpaceX's Falcon Heavy launch and triple booster landing at 8PM",
+      :description=>"Last year SpaceX finally launched its massive Falcon Heavy rocket for the first time. While the plan to return all three of its boosters fell just short of success, today the company will try again, and as usual you can watch a live stream of the attempt righ…",
+      :url=>"https://www.engadget.com/2019/04/10/falcon-heavy-spacex-launch/",
+      :urlToImage=>"https://o.aolcdn.com/images/dims?thumbnail=1200%2C630&quality=80&image_uri=https%3A%2F%2Fo.aolcdn.com%2Fimages%2Fdims%3Fcrop%3D5185%252C3098%252C0%252C0%26quality%3D85%26format%3Djpg%26resize%3D1600%252C956%26image_uri%3Dhttps%253A%252F%252Fs.yimg.com%252Fos%252Fcreatr-images%252F2019-04%252F7d0e4350-5bd1-11e9-afdf-77f7fbb2aa81%26client%3Da1acac3e1b3290917d92%26signature%3D06ad30bb9ffdb666590047003eff03e6d0d5c9f7&client=amp-blogside-v2&signature=8b1d0c4dcc151f7af0101934cfc734698c3c5a0b",
+      :publishedAt=>"2019-04-10T20:48:00Z",
+      :content=>"Last year SpaceX finally launched its massive Falcon Heavy rocket for the first time. While the plan to return all three of its boosters fell just short of success, today the company will try again, and as usual you can watch a live stream of the attempt righ… [+587 chars]"
+   }
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -11,4 +11,20 @@ describe Article, type: :model do
     it {should validate_presence_of(:published_at)}
     it {should validate_presence_of(:context)}
   end
+
+  describe 'instance methods' do
+    it 'can be intialized with article JSON' do
+      article_json = ApplicationHelper.single_article
+
+      article = Article.new(article_json)
+      expect(article.source).to eq(article_json[:source])
+      expect(article.author).to eq(article_json[:author])
+      expect(article.title).to eq(article_json[:title])
+      expect(article.description).to eq(article_json[:description])
+      expect(article.url).to eq(article_json[:url])
+      expect(article.url_to_image).to eq(article_json[:urlToImage])
+      expect(article.published_at).to eq(article_json[:publishedAt])
+      expect(article.content).to eq(article_json[:content])
+    end
+  end
 end
