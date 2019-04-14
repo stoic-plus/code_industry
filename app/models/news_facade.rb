71e7a710f6bf7b1ae089a1afd8a4bf2de3443aee
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-14 14:48:31 -0600

    adds helper method to NewsFacade to determine if article is relevant, adds test for filtering out irrelevant articles

--- app/models/news_facade.rb
+++ app/models/news_facade.rb
@@ -2,7 +2,19 @@ class NewsFacade
   def self.get_articles(query)
     articles_json = NewsService.everything_search(query)[:articles]
     articles_json.reduce([]) do |return_articles, article_json|
-      return_articles.push(NewsArticle.new(article_json))
+      if relevant_article?(article_json, query)
+        return_articles.push(NewsArticle.new(article_json))
+      end
+      return_articles
     end
+  end
+
+  private
+
+  def self.relevant_article?(article_json, query)
+    [:title, :description, :content].each do |attribute|
+      return true if article_json[attribute] =~ /#{Regexp.quote(query)}/i
+    end
+    return false
   end
 end
--- spec/facades/news_facade_spec.rb
+++ spec/facades/news_facade_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 describe NewsFacade, type: :facade do
   context '.articles' do
-    it 'returns article objects from NewsService given search string' do
+    before :each do
       expect(NewsService).to receive(:everything_search).with("spacex").and_return(ApplicationHelper.article_search_return)
+    end
+    it 'returns article objects from NewsService given search string' do
       articles = NewsFacade.get_articles("spacex")
-      expect(articles.count).to eq(1)
       expect(articles.first).to be_a(NewsArticle)
+    end
+    it 'removes articles that are not relevant from NewsService returned json' do
+      expect(ApplicationHelper.article_search_return[:articles].count).to eq(6)
+      articles = NewsFacade.get_articles("spacex")
+      expect(articles.count).to eq(3)
     end
   end
 end
