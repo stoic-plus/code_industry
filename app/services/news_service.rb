45073404208a847561a4c54bdeab7e0a1a3f4eaf
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-14 10:51:48 -0600

    adds NewS.everything_search, get_json and conn private helper methods

--- app/services/news_service.rb
+++ app/services/news_service.rb
@@ -1,3 +1,24 @@
 class NewsService
-  
+  def self.everything_search(search_query)
+    response = conn.get do |req|
+      req.url 'everything'
+      req.params['q'] = search_query
+      req.params['apiKey'] = ENV['API_KEY']
+      req.params['language'] = 'en'
+      req.params['sortBy'] = 'relevance'
+    end
+    get_json(response.body)
+  end
+
+  private
+
+  def self.get_json(response)
+    JSON.parse(response, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new(url: 'https://newsapi.org/v2') do |faraday|
+      faraday.adapter  Faraday.default_adapter
+    end
+  end
 end
--- spec/services/news_service_spec.rb
+++ spec/services/news_service_spec.rb
@@ -19,21 +19,23 @@ require 'rails_helper'
 #  },
 
 describe NewsService, type: :service do
-  it 'returns JSON given a search query' do
-    json = NewsService.single_search("spacex")
-
-    expect(json).to have_key(:status)
-    expect(json).to have_key(:totalResults)
-    expect(json).to have_key(:articles)
-    expect(json[:articles]).to have_key(:source)
-    expect(json[:articles].first[:source]).to have_key(:id)
-    expect(json[:articles].first[:source]).to have_key(:name)    
-    expect(json[:articles].first).to have_key(:author)
-    expect(json[:articles].first).to have_key(:title)
-    expect(json[:articles].first).to have_key(:description)
-    expect(json[:articles].first).to have_key(:url)
-    expect(json[:articles].first).to have_key(:urlToImage)
-    expect(json[:articles].first).to have_key(:publishedAt)
-    expect(json[:articles].first).to have_key(:content)
+  context '.everything_search' do
+    it 'returns JSON given a search query' do
+      json = NewsService.everything_search("spacex")
+      binding.pry
+      expect(json).to have_key(:status)
+      expect(json).to have_key(:totalResults)
+      expect(json).to have_key(:articles)
+      expect(json[:articles].first).to have_key(:source)
+      expect(json[:articles].first[:source]).to have_key(:id)
+      expect(json[:articles].first[:source]).to have_key(:name)
+      expect(json[:articles].first).to have_key(:author)
+      expect(json[:articles].first).to have_key(:title)
+      expect(json[:articles].first).to have_key(:description)
+      expect(json[:articles].first).to have_key(:url)
+      expect(json[:articles].first).to have_key(:urlToImage)
+      expect(json[:articles].first).to have_key(:publishedAt)
+      expect(json[:articles].first).to have_key(:content)
+    end
   end
 end
