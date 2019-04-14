1196f63b55ca8f2290802e3608f180033a6f2862
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-14 11:04:57 -0600

    adds default params to NS.everything_s

--- app/services/news_service.rb
+++ app/services/news_service.rb
@@ -1,11 +1,13 @@
 class NewsService
-  def self.everything_search(search_query)
+  def self.everything_search(search_query, sortBy='relevance', from, to)
     response = conn.get do |req|
       req.url 'everything'
-      req.params['q'] = search_query
+      req.params['q'] = "\"#{search_query}\""
+      req.params['from'] = from if from
+      req.params['to'] = to if to
       req.params['apiKey'] = ENV['API_KEY']
       req.params['language'] = 'en'
-      req.params['sortBy'] = 'relevance'
+      req.params['sortBy'] = sortBy
     end
     get_json(response.body)
   end
--- spec/services/news_service_spec.rb
+++ spec/services/news_service_spec.rb
@@ -1,28 +1,9 @@
 require 'rails_helper'
 
-# {
-# "status": "ok",
-# "totalResults": 5080,
-# -"articles": [
-# -{
-#    -"source": {
-#      "id": null,
-#      "name": "Dailydot.com"
-#    },
-#    "author": "Ellen Ioanes",
-#    "title": "Silk Road 2.0 founder sentenced to more than 5 years in prison",
-#    "description": "Thomas White was originally arrested in 2014. The post Silk Road 2.0 founder sentenced to more than 5 years in prison appeared first on The Daily Dot.",
-#    "url": "https://www.dailydot.com/debug/silk-road-2-thomas-white/",
-#    "urlToImage": "https://www.dailydot.com/wp-content/uploads/2019/04/dark-web-silk-road.jpg",
-#    "publishedAt": "2019-04-13T21:35:16Z",
-#    "content": "Thomas White, who launched the infamous Silk Road 2.0 site shortly after the original was taken down by the Federal Bureau of Investigation in 2013, was sentenced to 64 months in prison at Liverpool Crown Court on Friday, the BBC reports.\r\nWhite was known by … [+1498 chars]"
-#  },
-
 describe NewsService, type: :service do
   context '.everything_search' do
-    it 'returns JSON given a search query' do
+    it 'returns JSON given a single search query' do
       json = NewsService.everything_search("spacex")
-      binding.pry
       expect(json).to have_key(:status)
       expect(json).to have_key(:totalResults)
       expect(json).to have_key(:articles)
