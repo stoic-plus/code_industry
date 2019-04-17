07111fb10b24dfd9ddc5f9af143f740aed55828d
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-17 08:05:52 -0600

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,15 @@ Post.destroy_all
 Comment.destroy_all
 
 moderator = User.create(username: 'EL PATRON', name: 'steve', email: 's@mail.com', password: 'password', moderator: true)
-Topic.create(name: 'SpaceX', moderator_id: moderator.id)
-Topic.create(name: 'Blue Origin', moderator_id: moderator.id)
-Topic.create(name: 'Virgin Galactic', moderator_id: moderator.id)
+user = User.create(username: 'user', name: 'user', email: 'u@mail.com', password: 'password')
+spacex = Topic.create(name: 'SpaceX', moderator_id: moderator.id)
+blue = Topic.create(name: 'Blue Origin', moderator_id: moderator.id)
+virgin = Topic.create(name: 'Virgin Galactic', moderator_id: moderator.id)
 Topic.create(name: 'United Launch Alliance', moderator_id: moderator.id)
 Topic.create(name: 'NASA', moderator_id: moderator.id)
+FollowedTopic.create(user: user, topic: spacex)
+FollowedTopic.create(user: user, topic: blue)
+FollowedTopic.create(user: user, topic: virgin)
 
 Topic.create(name: 'Moon Base', moderator_id: moderator.id)
 Topic.create(name: 'Mars', moderator_id: moderator.id)
